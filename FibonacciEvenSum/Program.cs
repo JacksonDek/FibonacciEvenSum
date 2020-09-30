87ae5e21f4433200166e8ba21ef2096ed1351e65
JacksonDek <jackson_dekeyser@outlook.com>
2020-09-29 20:51:58 -0500

    This was attempt one using google for a simple solution

--- FibonacciEvenSum/Program.cs
+++ FibonacciEvenSum/Program.cs
@@ -11,6 +11,20 @@ namespace FibonacciEvenSum
         static void Main(string[] args)
         {
 
+            #region Reducecd Version
+            //long[] fib = { 2, 0 };
+            //int i = 0;
+            //long summed = 0;
+
+            //while (fib[i] < 4000000)
+            //{
+            //    summed += fib[i];
+            //    i = (i + 1) % 2;
+            //    fib[i] = 4 * fib[(i + 1) % 2] + fib[i];
+            //}
+
+            //Console.WriteLine(i);
+            #endregion
         }
     }
 }
